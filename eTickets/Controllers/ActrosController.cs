dec425658ced772e7920e39678ba93c206e4499e
EnesHetemi <eneshetemi3@gmail.com>
2022-08-27 22:28:58 +0200

    Shtimi i ActorsController

---
+++ eTickets/Controllers/ActrosController.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActrosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActrosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
