40bba6882c1f796231d3ee48154d118dc190eb22
EnesHetemi <eneshetemi3@gmail.com>
2022-08-28 19:23:42 +0200

    added generic getbyid including properties

--- eTickets/Data/Base/IEntityBaseRepository.cs
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -10,6 +10,8 @@ namespace eTickets.Data.Base
 
         Task<T> GetByIdAsync(int id);
 
+        Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includeProperties);
+
         Task AddAsync(T entity);
 
         Task UpdateAsync(int id, T entity);
