19976e81a7662a34df11d28b06f65beae7e2efcc
EnesHetemi <eneshetemi3@gmail.com>
2022-08-28 16:00:16 +0200

    Shtimi i configured producers service

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_con
 
 //Services Configuration
 builder.Services.AddScoped<IActorsService, ActorsService>();
-
+builder.Services.AddScoped<IProducersService, ProducersService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
